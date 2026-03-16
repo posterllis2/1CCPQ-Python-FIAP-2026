3838debd07109ea3ada98487b930f218edca687b
posterllis2 <posterlli.enzo@gmail.com>
2026-03-16 20:45:48 -0300

    aula 2 variaveis

---
+++ aula-02-variaveis-operadores/aula002a-variaveis.py
@@ -0,0 +1,27 @@
+'''
+print('Olá, Mundo')
+
+print(7 + 4)
+
+print('7 + 4')
+
+print('7'+'4')
+
+# Isso é o comentário de uma linha
+
+
+    Comentários de
+    múltiplas
+    linhas
+'''
+
+# Variáveis
+
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+peso = input('Digite seu peso: ')
+
+print(f'Olá, {nome}!')
+
+# IMPORTS --- SIMULAÇÃO DE UM FORMS NO CMD
+
---
+++ aula-02-variaveis-operadores/desafio 002.py
@@ -0,0 +1,5 @@
+dia = int(input("Qual o dia do seu nascimento? "))
+mes = int(input("Qual o dia do seu nascimento? "))
+ano = int(input("Qual o dia do seu nascimento? "))
+
+print(f"Você nasceu no dia {dia} de {mes} de {ano}!")
