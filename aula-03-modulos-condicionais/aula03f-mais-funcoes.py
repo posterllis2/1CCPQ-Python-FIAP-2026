ac9688d03715fc822d379858cafa463a9621ba6c
posterllis2 <posterllicc@gmail.com>
2026-04-06 19:42:34 -0300

    mais funções

---
+++ aula-03-modulos-condicionais/aula03f-mais-funcoes.py
@@ -0,0 +1,12 @@
+#FUNÇÃO COM PARAMETRO SEM RETORNO
+# def boas_vindas(nome):
+#     print(f"Olá, {nome}! Seja bem-vindo!")
+# nome_digitado = input("Digite seu nome: ")
+# boas_vindas(nome_digitado)
+
+# FUNÇÃO COM PARAMETRO COM RETORNO
+def soma(num_a, num_b):
+    soma = num_a + num_b
+    return soma
+resultado_soma = soma(1,2)
+print(resultado_soma)
